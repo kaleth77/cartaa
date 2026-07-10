db6b0cc1986b14936e5dc6125bb0350b97121d74
kaleth77 <kalecitopd@gmail.com>
2026-07-10 14:39:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("locked").style.display = "none";
-document.getElementById("letterSection").style.display = "flex";
-
-document.getElementById("letterText").innerHTML = `
-Hola, esta es una prueba.
-
-Si puedes leer esto, significa que el HTML y el CSS están bien.
-`;
